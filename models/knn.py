0ac710eda1259422d617211bb24a6bad5b21a42b
mustachemo <mhasan1080@gmail.com>
2024-12-08 16:13:19 -0800

    added results to json files

--- models/knn.py
+++ models/knn.py
@@ -1,10 +1,8 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import classification_report, accuracy_score
-import json
-import os
 
 # List of CSV files
 csv_files = [
@@ -32,53 +30,24 @@ def preprocess_data(data):
 
 
 def train(X_train, y_train):
-    model = SVC(kernel="linear", C=1, random_state=42)
+    model = KNeighborsClassifier(n_neighbors=3)
     model.fit(X_train, y_train)
     return model
 
 
 def evaluate_model(model, X_test, y_test):
     y_pred = model.predict(X_test)
-    accuracy = accuracy_score(y_test, y_pred)
-    report = classification_report(y_test, y_pred, output_dict=True)
-    print("Accuracy:", accuracy)
+    print("Accuracy:", accuracy_score(y_test, y_pred))
     print("Classification Report:\n", classification_report(y_test, y_pred))
-    return accuracy, report
-
-
-def save_results(file_name, model, accuracy, report):
-    results = {
-        "file_name": file_name,
-        "model_name": type(model).__name__,
-        "model_params": model.get_params(),
-        "accuracy": accuracy,
-        "classification_report": report,
-    }
-    result_file = f"results_{file_name.split('/')[-1].split('.')[0]}.json"
-
-    if os.path.exists(result_file):
-        with open(result_file, "r") as f:
-            existing_results = json.load(f)
-            if not isinstance(existing_results, list):
-                existing_results = [existing_results]
-    else:
-        existing_results = []
-
-    existing_results.append(results)
-
-    with open(result_file, "w") as f:
-        json.dump(existing_results, f, indent=4)
 
 
 if __name__ == "__main__":
-    print("Training SVM model")
     for file in csv_files:
-        print(f"data: {file}")
+        print(f"Training SVM model on {file}")
         print(f"number of features is {len(pd.read_csv(file).columns)}")
         print(f"size of the dataset is {len(pd.read_csv(file))}")
         data = load_data(file)
         X_train, X_test, y_train, y_test = preprocess_data(data)
-        model = train(X_train, y_train)
-        accuracy, report = evaluate_model(model, X_test, y_test)
-        save_results(file, model, accuracy, report)
+        svm_model = train(X_train, y_train)
+        evaluate_model(svm_model, X_test, y_test)
         print("-" * 50)
